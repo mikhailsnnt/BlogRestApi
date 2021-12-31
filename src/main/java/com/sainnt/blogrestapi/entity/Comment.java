1fcde77880cc889be6932b0784926886d7fe41fe
Mihail Nikitin <nitinin.miha@icloud.com>
2021-12-31 21:15:11 +0300

    Implemented Comment (Many to one Post) entity

---
+++ src/main/java/com/sainnt/blogrestapi/entity/Comment.java
@@ -0,0 +1,25 @@
+package com.sainnt.blogrestapi.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Entity
+@Table(name = "comments")
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private String email;
+    private String body;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+}
