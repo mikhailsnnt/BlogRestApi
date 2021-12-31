096efd6da6b9f27e041514d32389222782088aaa
Mihail Nikitin <nitinin.miha@icloud.com>
2021-12-31 13:13:37 +0300

    Initiated project structure

---
+++ src/main/java/com/sainnt/blogrestapi/BlogRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.sainnt.blogrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogRestApiApplication.class, args);
+    }
+
+}
