b3fc99d09040fe9fb15dd904ce0e00cd4b6307b8
Mihail Nikitin <nitinin.miha@icloud.com>
2022-01-01 18:02:59 +0300

    Initiated CommentDto

---
+++ src/main/java/com/sainnt/blogrestapi/dto/CommentDto.java
@@ -0,0 +1,11 @@
+package com.sainnt.blogrestapi.dto;
+
+import lombok.Data;
+
+@Data
+public class CommentDto {
+    private long id;
+    private String name;
+    private String email;
+    private String body;
+}
