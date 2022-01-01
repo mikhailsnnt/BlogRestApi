0d153906da0cce32e325f85c4cded6722e8c74d7
Mihail Nikitin <nitinin.miha@icloud.com>
2022-01-01 17:54:52 +0300

    Added nullable_false  to post_id column

--- src/main/java/com/sainnt/blogrestapi/entity/Comment.java
+++ src/main/java/com/sainnt/blogrestapi/entity/Comment.java
@@ -20,6 +20,6 @@ public class Comment {
     private String email;
     private String body;
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "post_id")
+    @JoinColumn(name = "post_id", nullable = false)
     private Post post;
 }
