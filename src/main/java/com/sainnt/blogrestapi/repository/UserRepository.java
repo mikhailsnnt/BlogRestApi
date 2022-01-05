9bee93d1d92445111eb95e79603e1565654c488e
Mihail Nikitin <nitinin.miha@icloud.com>
2022-01-05 21:46:17 +0300

    Created User and Role repositories

---
+++ src/main/java/com/sainnt/blogrestapi/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.sainnt.blogrestapi.repository;
+
+import com.sainnt.blogrestapi.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
+    Optional<User> findByUsernameOrEmail(String username,String email);
+    Boolean existsByUsername(String username);
+    Boolean existsByEmail(String email);
+}
